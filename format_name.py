39f6ed0678b190ccf615c4b5072f67838c54b65b
RollingBear <zhangshuo1120@hotmail.com>
2019-05-05 19:07:23 +0800

    add code

--- format_name.py
+++ format_name.py
@@ -5,21 +5,23 @@
 __author__ = 'RollingBear'
 
 import os
-import time
+import logging
+import traceback
 
 path = []
 name = []
 
 
-def getallfile(file_path):
+def get_file_list(file_path):
+    '''Traversing the file directory'''
     file_list = os.listdir(file_path)
-    # 遍历该文件夹下的所有目录或者文件
+    # Traversing the file directory
     for file in file_list:
         filepath = os.path.join(file_path, file)
-        # 如果是文件夹，递归调用函数
+        # if file folder, recursive
         if os.path.isdir(filepath):
-            getallfile(filepath)
-        # 如果不是文件夹，保存文件路径及文件名
+            get_file_list(filepath)
+        # if not file folder, save file name and path
         elif os.path.isfile(filepath):
             path.append(filepath)
             name.append(file)
@@ -27,19 +29,67 @@ def getallfile(file_path):
 
 
 def get(file_path):
+    '''empty list'''
     del path[:]
     del name[:]
-    return getallfile(file_path)
+    return get_file_list(file_path)
+
+
+def format_name_face(face, face_path):
+    '''rename face file'''
+    print(face)
+    face_format = face.split('_')
+    face_format_result = face_format[0] + '_' + face_format[2] + '0_face.jpg'
+    face_path_format = face_path.replace(str(face), str(face_format_result))
+    try:
+        os.rename(face_path, face_path_format)
+    except Exception as e:
+        logging.info(traceback.format_exc())
+
+
+def format_name_back(back, back_path):
+    '''rename back file'''
+    print(back)
+    back_format = back.split('_')
+    back_format_result = back_format[0] + '_' + back_format[2] + '0_back.jpg'
+    back_path_format = back_path.replace(back, back_format_result)
+    try:
+        os.rename(back_path, back_path_format)
+    except Exception as e:
+        logging.info(traceback.format_exc())
+
+
+def update_file(file_path):
+    '''Traversing the file directory, rename all file'''
+    get(file_path)
+    count = 0
+    for i in range(len(name)):
+        if 'FACE' in name[i]:
+            count += 1
+            format_name_face(name[i], path[i])
+        elif 'BACK' in name[i]:
+            count += 1
+            format_name_back(name[i], path[i])
+        else:
+            continue
+    print(count, 'picture formatted')
 
 
 if __name__ == '__main__':
     file_path = 'E:\\ftp\\admin'
-    get(file_path)
-    for num in range(len(path)):
-        print(path[num])
-        print(name[num])
-        print("created: %s" % time.ctime(os.path.getctime(path[num])))
-        print()
 
-    print(path)
-    print(name)
+    # get(file_path)
+    # print(path)
+    # print(name)
+
+    update_file(file_path)
+
+    # get(file_path)
+    # print(path)
+    # print(name)
+
+    # for num in range(len(path)):
+    #     print(path[num])
+    #     print(name[num])
+    #     print("created: %s" % time.ctime(os.path.getctime(path[num])))
+    #     print()
--- test.py
+++ test.py
@@ -4,27 +4,28 @@
 
 __author__ = 'RollingBear'
 
+str_face = '192.168.81.13_01_20190505160221929_FACE_SNAP.jpg'
+str_face_path = 'E:\\ftp\\admin\\192.168.81.13\\192.168.81.13_01_20190505160221929_FACE_SNAP.jpg'
+str_back = '192.168.81.13_01_20190505160221327_FACE_BACKGROUND.jpg'
+str_back_path = 'E:\\ftp\\admin\\192.168.81.13\\192.168.81.13_01_20190505160221327_FACE_BACKGROUND.jpg'
 
-def get_user(userfile):
-    # 定义一个用户列表
-    user_list = []
+str_face_format_1 = str_face.split('_')
+str_back_format_1 = str_back.split('_')
+str_face_format_2 = str_face_format_1[0] + '_' + str_face_format_1[2][:14] + str_face_format_1[2][15:17] + '0_face.jpg'
+str_back_format_2 = str_back_format_1[0] + '_' + str_back_format_1[2][:14] + str_face_format_1[2][15:17] + '0_back.jpg'
 
-    with open(userfile, encoding="utf-8") as f:
-        for line in f:
-            if not line.startswith('#') and line:
-                if len(line.split()) == 4:
-                    user_list.append(line.split())
-                else:
-                    print("user.conf配置错误")
-    return user_list
+print(str_face)
+print(str_face_format_2)
+print(str_back)
+print(str_back_format_2)
 
+# print(len('192.168.81.13_01_20190505170759875_FACE_SNAP.jpg'))
+# print(len('192.168.81.13_01_20190505170821281_FACE_BACKGROUND.jpg'))
 
-print(get_user('t2.py'))
+str_face_path_format = str_face_path.replace(str_face, str_face_format_2)
+str_back_path_format = str_back_path.replace(str_back, str_back_format_2)
 
-import mysql
-
-sql = mysql.mysql()
-
-listre = (sql.format_query_dict(_list_name='*', _table_name='user', _condition='user_id>0'))
-print(listre)
-print(listre[3][2])
+print(str_face_path)
+print(str_face_path_format)
+print(str_back_path)
+print(str_back_path_format)
